c1c9ba134649adc5d339317f893efbe4cd2c856d
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-25 01:37:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array2 = array[0..1]
+    array2.join(", ") << ", and #{array[2]}"
+  elsif array.length > 3
+    array2 = array[0..(array.length - 2)]
+    array2.join(", ") << ", and #{array[-1]}"
+  end
 end
