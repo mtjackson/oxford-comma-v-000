37e78b118c23e56d65ece7c3d673782759193114
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-25 01:36:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,5 +6,8 @@ def oxford_comma(array)
   elsif array.length == 3
     array2 = array[0..1]
     array2.join(", ") << ", and #{array[2]}"
+  elsif array.length > 3
+    array.length = length
+    array2 = array[0..]
   end
 end
