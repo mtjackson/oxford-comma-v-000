27277b6649d2fafff7a396b193d80f4479b52fad
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-25 01:29:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join(", ")
-  elsif array.length == 2
+  if array.length == 2
     array.join(" and ")
   elsif array.length == 3
     array2 = array[0..1]
